5f62428239087f33b7703c30d5bc9b9f4434f9a1
Ryan Williams <ryan@runsascoded.com>
2021-10-17 15:50:50 -0400

    rm toast stubs, add modal suppress box

--- js/annual-mtng-2021.js
+++ js/annual-mtng-2021.js
@@ -1,33 +1,16 @@
-var modalKey = '2021_annual_mtng_modal_shown'
+var modalKey = '2021_annual_mtng_modal_suppressed'
 function closeModal(){
     console.log("Closing modal")
-    // rv = false;
-    // event.defaultPrevented = rv;
-    //$('#annualModal2021').modal('hide');
-    // $('#annualModal2021').trigger('click');
-    // $('#annualModal2021').hide();
-    $.cookie(modalKey, true, { expires: 2147483647 });
-    // return rv;
+    var checked = $('#annualModalCheckbox2021').is(':checked')
+    console.log("checked:", checked);
+    if (checked) {
+        $.cookie(modalKey, true, {expires: 2147483647});
+    }
 }
-// var alertKey = '2021_annual_mtng_alert_shown'
-// function closeAlert(){
-//     console.log("Closing alert")
-//     $('#annualAlert2021').hide();
-//     $.cookie(alertKey, true, { expires: 2147483647 });
-// }
 $(document).ready(function(){
-    //$('.modal-header .close').on('click', closeModal);
     $('#annualModal2021').on('blur', closeModal);
     if ($.cookie(modalKey) == null) {
         console.log("Showing modal")
-        $('#annualModal2021').modal('show');  //show modal pop up
-        // console.log("Hiding alert")
-        // $('#annualAlert2021').hide();  //show modal pop up
-    } /*else if ($.cookie(alertKey) == null) {
-      console.log("Showing alert")
-      $('#annualAlert2021').show();  //show modal pop up
-    } else {
-      console.log("Hiding alert")
-      $('#annualAlert2021').hide();  //show modal pop up
-    }*/
+        $('#annualModal2021').modal('show');
+    }
 })

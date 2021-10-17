0a31ad0eb81dc3ca75c4c8f4e2fe72945155c9a5
Ryan Williams <ryan@runsascoded.com>
2021-10-17 13:31:21 -0400

    fix modal

--- js/annual-mtng-2021.js
+++ js/annual-mtng-2021.js
@@ -1,16 +1,22 @@
 var modalKey = '2021_annual_mtng_modal_shown'
-var alertKey = '2021_annual_mtng_alert_shown'
 function closeModal(){
     console.log("Closing modal")
-    $('#annualModal2021').modal('hide');
+    // rv = false;
+    // event.defaultPrevented = rv;
+    //$('#annualModal2021').modal('hide');
+    // $('#annualModal2021').trigger('click');
+    // $('#annualModal2021').hide();
     $.cookie(modalKey, true, { expires: 2147483647 });
+    // return rv;
 }
-function closeAlert(){
-    console.log("Closing alert")
-    $('#annualAlert2021').hide();
-    $.cookie(alertKey, true, { expires: 2147483647 });
-}
+// var alertKey = '2021_annual_mtng_alert_shown'
+// function closeAlert(){
+//     console.log("Closing alert")
+//     $('#annualAlert2021').hide();
+//     $.cookie(alertKey, true, { expires: 2147483647 });
+// }
 $(document).ready(function(){
+    //$('.modal-header .close').on('click', closeModal);
     $('#annualModal2021').on('blur', closeModal);
     if ($.cookie(modalKey) == null) {
         console.log("Showing modal")

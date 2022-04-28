8c6f7b1d7ab311839654f7f8521e0ddcb26b5004
Ryan Williams <ryan@runsascoded.com>
2022-04-28 17:32:33 -0400

    improve modal, add to home page

--- js/ecg-nyc-phl-2022-volunteer.js
+++ js/ecg-nyc-phl-2022-volunteer.js
@@ -4,17 +4,19 @@ var checkboxSelector = '#' + checkboxId
 var modalId = 'ecg-nyc-phl-2022-volunteer-modal'
 var modalSelector = '#' + modalId
 
-function closeModal(){
-    console.log("Closing modal")
+function setModalSuppressCookie() {
     var checked = $(checkboxSelector).is(':checked')
     console.log("checked:", checked);
     if (checked) {
         $.cookie(modalKey, true, {expires: 2147483647});
+    } else {
+        $.removeCookie(modalKey);
     }
+    console.log("set cookie:", $.cookie(modalKey))
 }
 
 $(document).ready(function(){
-    $(modalSelector).on('blur', closeModal);
+    $(modalSelector).on('blur', setModalSuppressCookie);
     if ($.cookie(modalKey) == null) {
         console.log("Showing modal")
         $(modalSelector).modal('show');
